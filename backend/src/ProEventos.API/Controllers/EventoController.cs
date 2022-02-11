2b5c15c5d975cd7930248234cf32476b824c58ec
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-02-11 08:57:33 -0300

    Using Ef and Created table Eventos

--- backend/src/ProEventos.API/Controllers/EventoController.cs
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,40 +13,22 @@ namespace ProEventos.API.Controllers
   [Route("api/[controller]")]
   public class EventoController : ControllerBase
   {
-    public IEnumerable<Evento> _evento = new Evento[] {
-      new Evento() {
-        EventoId = 1,
-        Tema = "TESTE",
-        Local = "Aqui",
-        Lote = "1",
-        QtdPessoa = 250,
-        DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-        ImagemURL = "img.png"
-      },
-      new Evento() {
-        EventoId = 2,
-        Tema = "TESTE2",
-        Local = "Aqui",
-        Lote = "1",
-        QtdPessoa = 250,
-        DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-        ImagemURL = "img.png"
-      }
-    };
-    public EventoController()
+    private readonly DataContext _context;
+    public EventoController(DataContext context)
     {
+      _context = context;
     }
 
     [HttpGet]
     public IEnumerable<Evento> Get()
     {
-      return _evento;
+      return _context.Eventos;
     }
 
     [HttpGet("{id}")]
-    public IEnumerable<Evento> GetById(int id)
+    public Evento GetById(int id)
     {
-      return _evento.Where(e => e.EventoId == id);
+      return _context.Eventos.FirstOrDefault(e => e.EventoId == id);
     }
   }
 }

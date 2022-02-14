7688a6a3bba51f770fde0c8d323c790255dfc822
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-02-14 14:01:19 -0300

    Creating filter to events

--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 100;
+  margImg: number = 2;
+  isCollapsed: boolean = true;
+  private _listFilter: string = '';
+
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    this.eventosFiltrados = this._listFilter ? this.filteredEvents(this.listFilter) : this.eventos;
+  }
+
+  filteredEvents(listFilter: string): any {
+    listFilter = listFilter.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: {tema: string; local: string}) =>
+        evento.tema.toLocaleLowerCase().indexOf(listFilter) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(listFilter) !== -1
+
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -18,7 +42,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       erro => console.log(erro)
     );
   }

d88eb4d148c04a26353f47c083f129d594d27f2b
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-02-14 17:58:49 -0300

    Created Persistence Interface

---
+++ backend/src/ProEventos.Persistence/IProEventosPersistence.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence
+{
+    public interface IProEventosPersistence
+    {
+        //GERAL
+         void Add<T>(T entity) where T: class;
+         void Update<T>(T entity) where T: class;
+         void Delte<T>(T entity) where T: class;
+         void DelteRange<T>(T[] entity) where T: class;
+
+         Task<bool> SaveChangesAsync();
+
+         //EVENTOS
+         Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
+         Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
+         Task<Evento> GetAllEventoByIdAsync(int EventoId, bool includePalestrantes);
+
+         //PALESTRANTES
+         Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos);
+         Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
+         Task<Palestrante> GetAllPalestranteByIdAsync(int PalestranteId, bool includeEventos);
+
+    }
+}

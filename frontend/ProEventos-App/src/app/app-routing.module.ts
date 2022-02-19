fbc2cf53ae7e03d2ee20ec098ff50803fa7cdf51
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-02-19 13:08:18 -0300

    creating registration  form

--- frontend/ProEventos-App/src/app/app-routing.module.ts
+++ frontend/ProEventos-App/src/app/app-routing.module.ts
@@ -7,9 +7,22 @@ import { EventoDetalheComponent } from './components/eventos/evento-detalhe/even
 import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
+import { UserComponent } from './components/user/user.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
+import { LoginComponent } from './components/user/login/login.component';
 
 const routes: Routes = [
+  {
+    path: 'user', component: UserComponent,
+    children: [
+      { path: 'login', component: LoginComponent, },
+      { path: 'registration', component: RegistrationComponent, },
+    ]
+  },
+  {
+    path: 'user/perfil', component: PerfilComponent
+  },
   { path: 'eventos', redirectTo: 'eventos/lista', pathMatch: 'full' },
   {
     path: 'eventos', component: EventosComponent,
@@ -22,7 +35,6 @@ const routes: Routes = [
   { path: 'contatos', component: ContatosComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'palestrantes', component: PalestrantesComponent },
-  { path: 'perfil', component: PerfilComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'dashboard', pathMatch: 'full' },
 ];

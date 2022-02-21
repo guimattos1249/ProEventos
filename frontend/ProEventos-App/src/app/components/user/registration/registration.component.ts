29c77c3851e627baa52916e99caf5378b0be0418
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-02-21 18:45:25 -0300

    reactivie forms

--- frontend/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ frontend/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
+import { ValidatorField } from './../../../helpers/ValidatorField';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  public form!: FormGroup;
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+    };
+
+    this.form = this.fb.group({
+      primeiroNome: ['', [Validators.required,  Validators.minLength(4), Validators.maxLength(50)]],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', [Validators.required, Validators.minLength(4)]],
+      senha: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      confirmeSenha: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      termos: ['', Validators.requiredTrue],
+    }, formOptions);
   }
 
 }

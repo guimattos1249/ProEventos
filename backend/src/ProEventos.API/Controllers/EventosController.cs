6bba3795794c937c5ef0f07fd815f24351adaee0
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-02-22 16:09:26 -0300

    fix on delete message

--- backend/src/ProEventos.API/Controllers/EventosController.cs
+++ backend/src/ProEventos.API/Controllers/EventosController.cs
@@ -107,7 +107,7 @@ namespace ProEventos.API.Controllers
                 if(evento == null) return NoContent();
 
                 return await _eventoService.DeleteEvento(id) ?
-                    Ok("Deletado") :
+                    Ok(new { message = "Deletado"}) :
                     throw new Exception("Ocorreu um problema não específico ao tentar deletar o Evento");
             }
             catch (Exception ex)

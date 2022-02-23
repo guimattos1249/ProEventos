c2072bd340fa018cffe9b759c36170f858028489
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-02-23 14:59:53 -0300

    Adding Lote routine crud

--- backend/src/ProEventos.API/Controllers/EventosController.cs
+++ backend/src/ProEventos.API/Controllers/EventosController.cs
@@ -21,81 +21,81 @@ namespace ProEventos.API.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-        try
-        {
-            var eventos = await _eventoService.GetAllEventosAsync(true);
-            if(eventos == null) return NoContent();
+            try
+            {
+                var eventos = await _eventoService.GetAllEventosAsync(true);
+                if(eventos == null) return NoContent();
 
-            return Ok(eventos);
-        }
-        catch (Exception ex)
-        {
-            return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
-        }
+                return Ok(eventos);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-        try
-        {
-            var evento = await _eventoService.GetEventoByIdAsync(id, true);
-            if(evento == null) return NoContent();
+            try
+            {
+                var evento = await _eventoService.GetEventoByIdAsync(id, true);
+                if(evento == null) return NoContent();
 
-            return Ok(evento);
-        }
-        catch (Exception ex)
-        {
-            return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar evento. Erro: {ex.Message}");
-        }
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar evento. Erro: {ex.Message}");
+            }
         }
 
         [HttpGet("{tema}/tema")]
         public async Task<IActionResult> GetByTema(string tema)
         {
-        try
-        {
-            var evento = await _eventoService.GetAllEventosByTemaAsync(tema, true);
-            if(evento == null) return NoContent();
+            try
+            {
+                var evento = await _eventoService.GetAllEventosByTemaAsync(tema, true);
+                if(evento == null) return NoContent();
 
-            return Ok(evento);
-        }
-        catch (Exception ex)
-        {
-            return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar evento. Erro: {ex.Message}");
-        }
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar evento. Erro: {ex.Message}");
+            }
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(EventoDto model)
         {
-        try
-        {
-            var evento = await _eventoService.AddEventos(model);
-            if(evento == null) return NoContent();
+            try
+            {
+                var evento = await _eventoService.AddEventos(model);
+                if(evento == null) return NoContent();
 
-            return Ok(evento);
-        }
-        catch (Exception ex)
-        {
-            return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar incluir evento. Erro: {ex.Message}");
-        }
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar incluir evento. Erro: {ex.Message}");
+            }
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, EventoDto model)
         {
-        try
-        {
-            var evento = await _eventoService.UpdateEvento(id, model);
-            if(evento == null) return NoContent();
+            try
+            {
+                var evento = await _eventoService.UpdateEvento(id, model);
+                if(evento == null) return NoContent();
 
-            return Ok(evento);
-        }
-        catch (Exception ex)
-        {
-            return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar alterar evento. Erro: {ex.Message}");
-        }
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar alterar evento. Erro: {ex.Message}");
+            }
         }
 
         [HttpDelete("{id}")]
---
+++ backend/src/ProEventos.API/Controllers/LotesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.Application.Dtos;
+using ProEventos.Application.Interfaces;
+using ProEventos.Domain;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LotesController : ControllerBase
+    {
+        private readonly ILoteService _loteService;
+        public LotesController(ILoteService loteService)
+        {
+            this._loteService = loteService;
+        }
+
+        [HttpGet("{eventoId}")]
+        public async Task<IActionResult> Get(int eventoId)
+        {
+            try
+            {
+                var lotes = await _loteService.GetLotesByEventoIdAsync(eventoId);
+                if(lotes == null) return NoContent();
+
+                return Ok(lotes);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar Lotes. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{eventoId}")]
+        public async Task<IActionResult> SaveLotes(int eventoId, LoteDto[] models)
+        {
+            try
+            {
+                var lotes = await _loteService.SaveLotes(eventoId, models);
+                if(lotes == null) return NoContent();
+
+                return Ok(lotes);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar salvar Lotes. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{eventoId}/{loteId}")]
+        public async Task<IActionResult> Delete(int eventoId, int loteId)
+        {
+            try
+            {
+                var lote = await _loteService.GetLoteByIdsAsync(eventoId, loteId);
+                if(lote == null) return NoContent();
+
+                return await _loteService.DeleteLote(lote.EventoId, lote.Id) ?
+                    Ok(new { message = "Deletado"}) :
+                    throw new Exception("Ocorreu um problema não específico ao tentar deletar o Lotes");
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar deletar Lotes. Erro: {ex.Message}");
+            }
+        }
+    }
+}

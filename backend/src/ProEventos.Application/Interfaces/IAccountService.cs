45bcb01fd577a81e49aaa7a94c1acab6a4c5c603
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-03-09 13:02:06 -0300

    Creating UserDto's and IAccountService

---
+++ backend/src/ProEventos.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using ProEventos.Application.Dtos;
+
+namespace ProEventos.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<bool> UserExists(string username);
+        Task<UserUpdateDto> GetUserByUsernameAsync(string username);
+        Task<SignInResult> CheckUserPasswordAsync(UserUpdateDto userUpdateDto, string Password);
+        Task<UserDto> CreatAccountAsync(UserDto userDto);
+        Task<UserUpdateDto> UpdateAccount(UserUpdateDto userUpdateDto);
+    }
+}

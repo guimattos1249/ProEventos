c645c8a6962fd1c34be7196e1fd68821f1e40503
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-03-09 12:51:28 -0300

    Creating and using User Interface

---
+++ backend/src/ProEventos.Persistence/UserPersist.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain.Identity;
+using ProEventos.Persistence.Contexts;
+using ProEventos.Persistence.Interfaces;
+
+namespace ProEventos.Persistence
+{
+    public class UserPersist : GeralPersist, IUserPersist
+    {
+        private readonly ProEventosContext _context;
+
+        public UserPersist(ProEventosContext context) : base(context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(int Id)
+        {
+            return await _context.Users.FindAsync(Id);
+        }
+
+        public async Task<User> GetUserByUsernameAsync(string Username)
+        {
+            return await _context.Users
+                                    .SingleOrDefaultAsync(user => user.UserName == Username.ToLower());
+        }
+    }
+}

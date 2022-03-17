066a953fa1c8b61cc2bc1f65e8ed0173ad455c4b
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-03-17 17:59:03 -0300

    using token, will be refactor

--- frontend/ProEventos-App/src/app/services/evento.service.ts
+++ frontend/ProEventos-App/src/app/services/evento.service.ts
@@ -1,6 +1,6 @@
 import { environment } from './../../environments/environment';
 import { Evento } from './../models/Evento';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
@@ -10,10 +10,12 @@ import { take } from 'rxjs/operators';
 )
 export class EventoService {
   baseURL = environment.apiURL + 'api/eventos';
+  tokenHeader = new HttpHeaders({ 'Authorization': 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwidW5pcXVlX25hbWUiOiJvZ3VpbWF0dG9zIiwibmJmIjoxNjQ3NTQ3NjEwLCJleHAiOjE2NDgxNTI0MTAsImlhdCI6MTY0NzU0NzYxMH0.814ecKfQIo8HlQ-hSmVsfMw5H9i-wdEakOtp2dapXOIocZf6Cail0XWQWKva_wWQBeAK8ynIxWejL5WERME68g' });
+
   constructor(private http: HttpClient) { }
 
   public getEventos(): Observable<Evento[]> {
-    return this.http.get<Evento[]>(this.baseURL)
+    return this.http.get<Evento[]>(this.baseURL, { headers: this.tokenHeader })
       .pipe(take(1));
   }
   public getEventosByTema(tema: string): Observable<Evento[]> {
